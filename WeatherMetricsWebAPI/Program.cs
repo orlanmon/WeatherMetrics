54f5d52cb09035522d9b275336c3e3523441788c
Orlando Monaco <monacos@monacos.us>
2025-10-21 09:45:10 -0400

    Added CORS Logic

--- WeatherMetricsWebAPI/Program.cs
+++ WeatherMetricsWebAPI/Program.cs
@@ -40,10 +40,58 @@ namespace WeatherMetricsWebAPI
             });
 
 
-
-
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+
+
+            // Conifgure CORS Settings
+
+            builder.Services.AddCors(options =>
+            {
+                // Configure SpecificOriginsPolicy
+                var specificOriginsSection = builder.Configuration.GetSection("CorsSettings:SpecificOriginsPolicy");
+                if (specificOriginsSection.Exists())
+                {
+                    var allowedOrigins = specificOriginsSection["AllowedOrigins"]?.Split(';', StringSplitOptions.RemoveEmptyEntries);
+                    var allowedHeaders = specificOriginsSection["AllowedHeaders"]?.Split(';', StringSplitOptions.RemoveEmptyEntries);
+                    var allowedMethods = specificOriginsSection["AllowedMethods"]?.Split(';', StringSplitOptions.RemoveEmptyEntries);
+                    var allowCredentials = specificOriginsSection.GetValue<bool>("AllowCredentials");
+
+                    options.AddPolicy("SpecificOriginsPolicy", policy =>
+                    {
+                        if (allowedOrigins != null && allowedOrigins.Length > 0)
+                        {
+                            policy.WithOrigins(allowedOrigins);
+                        }
+                        if (allowedHeaders != null && allowedHeaders.Length > 0)
+                        {
+                            policy.WithHeaders(allowedHeaders);
+                        }
+                        if (allowedMethods != null && allowedMethods.Length > 0)
+                        {
+                            policy.WithMethods(allowedMethods);
+                        }
+                        if (allowCredentials)
+                        {
+                            policy.AllowCredentials();
+                        }
+                    });
+                }
+
+                // Configure AnyOriginPolicy (if needed)
+                var anyOriginSection = builder.Configuration.GetSection("CorsSettings:AnyOriginPolicy");
+                if (anyOriginSection.Exists() && anyOriginSection.GetValue<bool>("AllowAnyOrigin"))
+                {
+                    options.AddPolicy("AnyOriginPolicy", policy =>
+                    {
+                        policy.AllowAnyOrigin()
+                              .AllowAnyHeader()
+                              .AllowAnyMethod();
+                    });
+                }
+            });
+
+
 
             var app = builder.Build();
 
@@ -64,6 +112,10 @@ namespace WeatherMetricsWebAPI
 
 
             app.MapControllers(); // Maps attribute-routed controllers
+
+
+            app.UseCors();
+
 
 
             // Configure AutoMapper For Model to DTOs 

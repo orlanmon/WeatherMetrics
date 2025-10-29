8699538bfc42c383b000e75c9f6383d47026b6f1
Orlando Monaco <monacos@monacos.us>
2025-10-29 15:19:43 -0400

    Angular Web UI Changes

--- WeatherMetricsWeb/src/app/features/weathermetricsdevices/components/weathermetricsdevices/weathermetricsdevices.component.ts
+++ WeatherMetricsWeb/src/app/features/weathermetricsdevices/components/weathermetricsdevices/weathermetricsdevices.component.ts
@@ -1,17 +1,67 @@
 import { Component } from '@angular/core';
+import { WeathermetricsdevicesService } from '../../services/weathermetricsdevices/weathermetricsdevices.service';
 
 @Component({
   selector: 'app-weathermetricsdevices',
   imports: [],
   templateUrl: './weathermetricsdevices.component.html',
-  styleUrl: './weathermetricsdevices.component.css'
+  styleUrl: './weathermetricsdevices.component.css',
+  providers: [WeathermetricsdevicesService],
 })
 export class WeathermetricsdevicesComponent {
 
-constructor() {
+private weathermetricsdevicesServ : WeathermetricsdevicesService;
+isEnabled? : boolean;
+strIoTDevice? : string;
+strIoTDeviceUptime? : string;
 
 
-  
+constructor(private weathermetricsdevicesService : WeathermetricsdevicesService ) {
+
+  this.weathermetricsdevicesServ = weathermetricsdevicesService;
+
 }
+
+
+ ngOnInit() {
+
+    if ( this.isEnabled == undefined ) {
+
+    } else {
+
+    }
+    
+  }
+
+  onEnable() {
+
+    // this.weathermetricsdevicesServ
+
+
+    console.log("Weather Metrics Device Enable")
+
+    this.isEnabled = true;
+
+  }
+
+  onDisable() {
+
+    // this.weathermetricsdevicesServ
+
+    console.log("Weather Metrics Device Disable")
+
+    this.isEnabled = false;
+
+  }
+
+  onDeviceSelectionChange(event: Event) {
+
+    console.log("Weather Metrics Device Change")
+
+    // Query Device Status
+
+    // Update UI For Device Status
+
+  }
 
 }
